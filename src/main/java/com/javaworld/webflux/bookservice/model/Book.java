2c512856670a6227c307906511e9adc77768166f
illichso <illichso@gmail.com>
2018-09-05 15:02:33 +0200

    add classes

---
+++ src/main/java/com/javaworld/webflux/bookservice/model/Book.java
@@ -0,0 +1,60 @@
+package com.javaworld.webflux.bookservice.model;
+
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+
+    public Book() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (getId() != null ? !getId().equals(book.getId()) : book.getId() != null) return false;
+        if (getTitle() != null ? !getTitle().equals(book.getTitle()) : book.getTitle() != null) return false;
+        return getAuthor() != null ? getAuthor().equals(book.getAuthor()) : book.getAuthor() == null;
+    }
+
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getTitle() != null ? getTitle().hashCode() : 0);
+        result = 31 * result + (getAuthor() != null ? getAuthor().hashCode() : 0);
+        return result;
+    }
+
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/javaworld/webflux/bookservice/service/BookService.java
@@ -0,0 +1,15 @@
+package com.javaworld.webflux.bookservice.service;
+
+import com.javaworld.webflux.bookservice.model.Book;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface BookService {
+    Mono<Book> findById(String id);
+
+    Flux<Book> findAll();
+
+    Mono<Book> save(Book book);
+
+    Mono<Void> deleteById(String id);
+}
